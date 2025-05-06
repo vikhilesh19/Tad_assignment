a239e29353aee7c5c2fc2d2b0af15442a1e9cbf3
vikhilesh19 <cs24b1077@iiitdm.ac.in>
2025-05-07 00:32:21 +0530

    New

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+#Fibonacci Number
+n=int(input("Enter n to find nth fibonacci number : "))
+if(n<=0):
+    print("Enter a valid n value")
+elif(n==1):
+    print("1st fibonacci number is : 0")
+else:
+    a=0
+    b=1
+    for i in range (2,n):
+        c=a+b
+        a=b
+        b=c
+    print(f"{n}th fibonacci number is : {c}")
